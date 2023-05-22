9d40938b7de7ff48ec965ecdbb05018aa96594b2
Mirjalol <jalolubaydullayev575@gmail.com>
2023-05-22 10:09:57 +0500

    04

--- bin/basic_function_parameters04.dart
+++ bin/basic_function_parameters04.dart
@@ -1,2 +1,11 @@
 // Description: Create a function named "is_even" that takes an integer as a parameter and returns true if the number is even, and false otherwise.
-void main() {}
+bool is_even(int a){
+    
+    if(a % 2 == 0){
+        return false;
+    }
+    return true;
+}
+void main() {
+    print(is_even(6));
+}
