200494606718898a3e58ed157e5a5addd3300cf5
Mirjalol <jalolubaydullayev575@gmail.com>
2023-05-22 10:09:37 +0500

    03

--- bin/basic_function_parameters03.dart
+++ bin/basic_function_parameters03.dart
@@ -1,3 +1,7 @@
 // Description: Create a function named "greet_person" that takes a person's name (String) as a parameter and returns a greeting message including the person's name.
-
-void main() {}
+String greet_person(String s){
+    return 'hello ' + s;
+}
+void main() {
+    print(greet_person('mirjalol'));
+}
