da01eef61da11a30cd068344da0dc11d47c42651
Mirjalol <jalolubaydullayev575@gmail.com>
2023-05-22 10:09:23 +0500

    02

--- bin/basic_function_parameters02.dart
+++ bin/basic_function_parameters02.dart
@@ -1,3 +1,7 @@
 // Description: Create a function named "calculate_area" that takes the radius of a circle (double) as a parameter and returns the area of the circle.
-
-void main() {}
+double calculate_area(double a){
+    return a;
+}
+void main() {
+    print(calculate_area(4.50));
+}
