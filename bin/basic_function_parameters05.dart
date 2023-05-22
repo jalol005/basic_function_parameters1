0d2ceca0adbc9eec1526c64a5a86868ef9815e75
Mirjalol <jalolubaydullayev575@gmail.com>
2023-05-22 10:10:11 +0500

    05

--- bin/basic_function_parameters05.dart
+++ bin/basic_function_parameters05.dart
@@ -1,2 +1,7 @@
 // Description: Create a function named "multiple" that takes a number1 (double) and a number2 (int) as parameters and returns the result of multiplacation of two parameters.
-void main() {}
+int multiple(int a,int b){
+    return a * b;
+}
+void main() {
+    print(multiple(3, 3));
+}
